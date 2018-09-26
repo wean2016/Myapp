3393390891bd6875455ddddc2b288f8652b966bf
wean <weanyq@gmail.com>
2018-09-26 17:18:00 +0800

    修复去重，完成批量生成表达式

--- src/main/scala/Question.scala
+++ src/main/scala/Question.scala
@@ -1,3 +1,6 @@
+import java.util.NoSuchElementException
+
+import scala.collection.mutable
 import scala.util.Random
 
 /**
@@ -10,26 +13,26 @@ object Question {
 
   /**
     * 构造表达式
-    * @param sum 剩余符号数
+    * @param symbolSum 剩余符号数
     * @param bound 表达式钟的数字不大于 bound
     * @return 生成的节点树的根
     */
-  def createNodeTree(sum:Int, bound:Int):Node = {
-    if (sum == 0){
+  def createNodeTree(symbolSum:Int, bound:Int):Node = {
+    if (symbolSum == 0){
       // 中间节点构造完成，生成叶子节点
       Node(Number.randomNumber(bound), null, null, 0, null)
     }else {
-      val left = Random.nextInt(sum)
-      val right = sum - left - 1
+      val left = Random.nextInt(symbolSum)
+      val right = symbolSum - left - 1
       var leftNode = createNodeTree(left,bound)
       var rightNode = createNodeTree(right,bound)
       // 本节点的符号
-      val symbol = Symbol.values()(Random.nextInt(Symbol.values().length))
-      // 如果是除号且右节点是 0 ，那么交换左右节点
+      var symbol = Symbol.values()(Random.nextInt(Symbol.values().length))
+      // 如果是除号且右节点是 0 ，那么重新生成一个不是除号的符号
       if (symbol == Symbol.DIV && rightNode.value.a == 0){
-        val tempNode = leftNode
-        leftNode = rightNode
-        rightNode = tempNode
+        while (symbol == Symbol.DIV){
+          symbol = Symbol.values()(Random.nextInt(Symbol.values().length))
+        }
       }
       // 本节点的结果
       var value = Calculate.calc(leftNode.value,rightNode.value,symbol).get
@@ -44,6 +47,14 @@ object Question {
       // 生成本节点
       Node(value,leftNode,rightNode,high,symbol)
     }
+  }
+
+  def batchNodeGenerator(sum:Int, symbolSum:Int, bound:Int) ={
+    val resultSet:mutable.HashSet[Node] = mutable.HashSet()
+    while (resultSet.size < sum){
+      resultSet.add(createNodeTree(Random.nextInt(symbolSum) + 1,bound))
+    }
+    resultSet
   }
 
 }
